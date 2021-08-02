ee67573a5e5d78118fca9944de0d036bb9b26a19
Obed Mulumba <ommartiste007@gmail.com>
2021-08-02 03:25:11 +0200

    added app.blade.php and other files

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\StudentController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::middleware('api')->group(function () {
+    Route::resource('students', StudentController::class);
 });
